c12eb8293559426e0fb8f700be178be787e2f16f
AnikIslam51 <anik.elahi@gmail.com>
2025-12-21 12:40:10 +0600

    Add string.py file

---
+++ string.py
@@ -0,0 +1,17 @@
+a= 'Checking the Strings with single quote'
+b= "Checking the Strings with double quote"
+c= """Checking 
+the Strings with 
+tripple quote"""
+
+print(a)
+print(b)
+print(c)
+
+print(type(a))
+print(type(b))
+print(type(c))
+
+print(a+b+c)
+print(a+" "+b+" "+c)
+print(a,b,c)
