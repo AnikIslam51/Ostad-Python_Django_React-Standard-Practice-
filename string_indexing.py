5e107bddebe847ee0dae73ac06653919a5bdebf3
AnikIslam51 <anik.elahi@gmail.com>
2025-12-21 13:41:13 +0600

    Add string_indexing.py file

---
+++ string_indexing.py
@@ -0,0 +1,13 @@
+a="I don't like you Pooshpo!"
+
+print(a)
+print(a[0],a[2],a[5])
+
+#Finding Length
+print(len(a))
+
+#Finding Last Value of String
+print(a[len(a)-1])
+
+#Negative Indexing (Finding Last value)
+print(a[-1])
