26941694a0720ef5cbe6604f725ab0878bdc006d
AnikIslam51 <anik.elahi@gmail.com>
2025-12-23 00:18:28 +0600

    Add math_function.py file

---
+++ math_function.py
@@ -0,0 +1,13 @@
+import math
+
+# Ceil
+x = 4.51
+print(math.ceil(x))
+
+# Floor
+print(math.floor(x))
+
+# Round
+# যদি কোনো সংখ্যা .00 থেকে .50 এর মধ্যে থাকে তাহলে floor
+# যদি কোনো সংখ্যা .51 থেকে .99 এর মধ্যে থাকে তাহলে ceil
+print(round(x))
