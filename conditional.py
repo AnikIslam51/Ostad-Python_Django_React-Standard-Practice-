643fcbab39360a4788785326785b211d73e0cfd3
AnikIslam51 <anik.elahi@gmail.com>
2025-12-23 00:37:15 +0600

    Add conditional.py file

---
+++ conditional.py
@@ -0,0 +1,13 @@
+print(6==10)
+print(10==10)
+
+x = int(input("Enter an integer value between 1 to 100: "))
+
+if x > 0 and x <= 50 :
+    print("School e jabo")
+
+elif x > 50 and x <= 100 :
+    print("Khelte jabo")
+    
+else:
+   print("Ghumabo") 
